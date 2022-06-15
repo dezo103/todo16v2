b8f277f7920bee1f3553e223754199d5dfafdeef
Aliaksandr Rudyk <dezo103@gmail.com>
2022-06-15 20:48:39 +0300

    fix test errors

--- src/features/TodolistsList/task-reducer.test.ts
+++ src/features/TodolistsList/task-reducer.test.ts
@@ -1,6 +1,6 @@
 import {addTaskTC, fetchTasksTC, removeTaskTC, tasksReducer, TasksStateType, updateTaskTC} from './tasks-reducer'
 
-import {addTodolistAC, removeTodolistAC, fetchTodolistsTC, removeTodolistTC} from './todolists-reducer'
+import {addTodolistAC, fetchTodolistsTC, removeTodolistTC} from './todolists-reducer'
 import {TaskPriorities, TaskStatuses} from '../../api/todolists-api'
 
 let startState: TasksStateType = {};
@@ -103,7 +103,7 @@ test('new array should be added when new todolist is added', () => {
     expect(endState[newKey]).toEqual([]);
 });
 test('propertry with todolistId should be deleted', () => {
-    const action = removeTodolistTC.fulfilled({id: "todolistId2"}, 'requestId', "todolistId2");
+    const action = removeTodolistTC.fulfilled({id: "todolistId2"}, 'requestId', {todolistId: "todolistId2"});
 
     const endState = tasksReducer(startState, action)
 
